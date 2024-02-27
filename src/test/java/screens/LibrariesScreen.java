b8b930e7e98aa176aa18e987ab0c9829a9739e39
AEkaterina <astapkina.caterina@gmail.cpm>
2024-02-27 19:49:45 +0400

    updated distributors

--- src/test/java/screens/LibrariesScreen.java
+++ src/test/java/screens/LibrariesScreen.java
@@ -1,6 +1,5 @@
 package screens;
 
-import aquality.appium.mobile.application.AqualityServices;
 import aquality.appium.mobile.elements.ElementType;
 import aquality.appium.mobile.elements.interfaces.IButton;
 import aquality.appium.mobile.elements.interfaces.ILabel;
@@ -9,12 +8,9 @@ import com.google.common.collect.Ordering;
 import framework.utilities.ActionProcessorUtils;
 import framework.utilities.LocatorUtils;
 import framework.utilities.swipe.SwipeElementUtils;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.offset.PointOption;
 import models.AndroidLocator;
 import models.IosLocator;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +24,7 @@ public class LibrariesScreen extends Screen {
             new AndroidLocator(By.xpath("//android.widget.Button[@text=\"Remove\"]")),
             new IosLocator(By.xpath("//XCUIElementTypeButton[@label=\"Delete\"]"))), "Delete library button");
 
-    private static final String LIBRARY_NAME_LOC_IOS = "//XCUIElementTypeStaticText[@name=\"%s\"]/parent::XCUIElementTypeCell";
+    private static final String LIBRARY_NAME_LOC_IOS = "//XCUIElementTypeTable/XCUIElementTypeCell/XCUIElementTypeStaticText[@name=\"%s\"]";
     private static final String LIBRARY_LOC_IOS = "//XCUIElementTypeTable/XCUIElementTypeCell";
     private static final String LIBRARY_NAME_ON_LIBRARY_SETTINGS_LOC_IOS = "//XCUIElementTypeStaticText[@name=\"%s\"]";
     private static final String LIBRARY_CELL_BY_LIBRARY_NAME_LOC_IOS = "//XCUIElementTypeStaticText[@name=\"%s\"]/parent::XCUIElementTypeCell";
@@ -59,12 +55,7 @@ public class LibrariesScreen extends Screen {
     }
 
     public void openLibrary(String libraryName) {
-        ActionProcessorUtils.doForAndroid(() -> getLibraryButton(libraryName).click());
-        ActionProcessorUtils.doForIos(() -> {
-            Point point = getLibraryButton(libraryName).getElement().getCenter();
-            TouchAction action = new TouchAction(AqualityServices.getApplication().getDriver());
-            action.tap(PointOption.point(point)).perform();
-        });
+        getLibraryButton(libraryName).click();
     }
 
     public boolean isAddLibraryBtnDisplayed() {
