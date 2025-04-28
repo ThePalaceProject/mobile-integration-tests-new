4f8d0308f80f151de338f858415089b3ec6ec3be
Daniel Bernstein <dabylon@gmail.com>
2025-04-28 14:19:29 -0700

    Revert "Update TestRunner.java"
    This reverts commit acd802fe30ee902b45c65f8be939cae6510fe63d.

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,6 @@ import framework.utilities.feedxmlutil.GettingBookUtil;
 import framework.utilities.feedxmlutil.XMLUtil;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.qameta.allure.Allure; //Import Allure
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
@@ -29,19 +28,10 @@ public class TestRunner {
 
         @BeforeClass
         public static void setup() {
-                // Fetch the BrowserStack app link from the system property (from the Maven command line)
-                String bsAppLink = System.getProperty("bs_app_link", "bs://c18a7bd4186aa3384596153ab08de6e3f157d350"); // Default URL if not set
-
-                // Log the BrowserStack app URL to Allure
-                Allure.addAttachment("BrowserStack App URL", bsAppLink); // Attach to Allure report
-
-                // Existing setup logic
                 AqualityServices.getLogger().info("Start getting books");
                 XMLUtil xmlUtil = new XMLUtil();
                 GettingBookUtil.setXmlUtil(xmlUtil);
                 GettingBookUtil.printDistributorsInfo();
                 AqualityServices.getLogger().info("end getting books");
-    }
-}
         }
 }
